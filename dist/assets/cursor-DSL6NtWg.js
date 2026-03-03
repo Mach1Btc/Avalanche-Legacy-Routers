2bc1efb630ded6c7465ed4efeaf8116ed40179d2
mach1btc <allxcaps@gmail.com>
2026-03-03 12:48:14 -0600

    fix bottom bar formatting

--- dist/assets/cursor-DSL6NtWg.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./core-DXaSRoOB.js";import"./index-C8VxEKkg.js";import"./index.es-BI3kXSmK.js";import"./events-DQ172AOg.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
